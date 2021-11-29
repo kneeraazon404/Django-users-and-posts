1a600bd663ad2eb950fd71ce2545b8682ad04c7d
kneeraazon01 <kneeraazon@gmail.com>
2021-11-30 03:02:49 +0545

    first commit haha

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-django app that lets the users register, login and make posts
-
-# here the  link to the  demo app
-
-link : https://usersandposts.herokuapp.com/
+FlexyLite Web App
--- django_go0/settings.py
+++ django_go0/settings.py
@@ -9,7 +9,7 @@ DEBUG = "True"
 
 ALLOWED_HOSTS = ["*"]
 
-
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
     "users.apps.UsersConfig",
@@ -90,10 +90,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
@@ -117,9 +113,5 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-
-# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 django_heroku.settings(locals())
